37431d8515d08f4d892af8ba8aed736a4987e629
reyangga9 <reyangga5@gmail.com>
2022-04-07 23:02:48 +0700

    hasil

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    DEFAULT: "#0D0D2B",
+                    icon: "#3671E9",
+                    button: "#3671E9",
+                    white10: "#FFFFFF1A"
+                }
+            },
+        },
+    },
+    plugins: [],
+}
